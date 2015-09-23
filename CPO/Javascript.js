63203411ccf055778efaaaab1a05c01cc468aa3d
nj-benliu <nj.benliu@gmail.com>
2015-09-23 16:04:44 -0400

    Update Javascript.js

--- CPO/Javascript.js
+++ CPO/Javascript.js
@@ -4,11 +4,11 @@ document.getElementById("date").innerHTML = date;
 }
 
 function valid(){
-  x = getElementById("Number").value
+  x = getElementById("number").value
   if x !=== -1{
   text = "INCORRECT!"
   } else{
     text = "So, we will all program the induvidual programs, and I will put them together."
   }
-  ducoment.getElementbyId("Info").innerHTML = text;
+  ducoment.getElementbyId("1nfo").innerHTML = text;
 }
