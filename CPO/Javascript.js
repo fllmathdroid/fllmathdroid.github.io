8a8a88dca9421ee16cdeba8b3cedcb7a5111601b
nj-benliu <nj.benliu@gmail.com>
2015-09-23 15:56:41 -0400

    Update Javascript.js

--- CPO/Javascript.js
+++ CPO/Javascript.js
@@ -2,3 +2,13 @@ function date(){
 var date = new Date();
 document.getElementById("date").innerHTML = date;
 }
+
+function valid(){
+  x = getElementById("Number").value
+  if x == -1{
+  text = "So, we will all program the induvidual programs, and I will put them together."
+  } else{
+    text = "INCORRECT!"
+  }
+  ducoment.getElementbyId("Info").innerHTML = text;
+}
