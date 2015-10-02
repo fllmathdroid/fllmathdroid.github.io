4016be2fa1d5ad56ea4392a8adbbf9d3eeb7fa7b
ev3commander <ev3commander@users.noreply.github.com>
2015-10-02 19:26:13 -0400

    fix, semicolons

--- CPO/javascript.js
+++ CPO/javascript.js
@@ -1,9 +1,9 @@
 function valid(){
-  x = getElementById("number").value
-  if x !=== -1{
-  text = "INCORRECT!"
+  x = getElementById("number").value;
+  if x !== -1{
+  text = "INCORRECT!";
   } else{
-    text = "So, we will all program the induvidual programs, and I will put them together. GOT IT?"
+    text = "So, we will all program the induvidual programs, and I will put them together. GOT IT?";
   }
   ducoment.getElementbyId("info").innerHTML = text;
 }
