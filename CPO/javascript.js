9c477472a61d5cd2eaabb7fbc38c1d58b8bde6ad
nj-benliu <nj.benliu@gmail.com>
2015-10-24 10:07:00 -0400

    Delete javascript.js

--- CPO/javascript.js
+++
@@ -1,9 +0,0 @@
-function valid(){
-  x = getElementById("number").value;
-  if x !== -1{
-  text = "INCORRECT!";
-  } else{
-    text = "So, we will all program the induvidual programs, and I will put them together. GOT IT?";
-  }
-  ducoment.getElementbyId("info").innerHTML = text;
-}
